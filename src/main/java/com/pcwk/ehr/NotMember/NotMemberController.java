572c26ba928159c2510aca4ff9085f89eeb36a4b
최유정 <133944035+cheezcyj@users.noreply.github.com>
2023-06-19 17:49:39 +0900

    수정

--- src/main/java/com/pcwk/ehr/NotMember/NotMemberController.java
+++ src/main/java/com/pcwk/ehr/NotMember/NotMemberController.java
@@ -1,5 +1,15 @@
 package com.pcwk.ehr.NotMember;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class NotMemberController {
 
+	@RequestMapping("/")
+    public String showForm() {
+        return "input-form";
+    }
+	
 }
--- src/main/java/com/pcwk/ehr/NotMember/NotMemberDao.java
+++ src/main/java/com/pcwk/ehr/NotMember/NotMemberDao.java
@@ -1,10 +1,5 @@
 package com.pcwk.ehr.NotMember;
 
-public class NotMemberDao {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+public interface NotMemberDao {
 
 }
